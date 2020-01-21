940e3c1096e20fdf69ac54f613ab87dc1d7a9722
Kurt LaVacque <lavacquek@icloud.com>
2020-01-21 16:49:59 -0600

    Got the manager informaiton to run only once while the other ones run as many times as you want

--- app.js
+++ app.js
@@ -2,24 +2,74 @@
 
 // Calling inquirer for the terminal based interface
 const inquirer = require("inquirer");
-// const Engineer = require("./lib/Engineer");
-// const Intern = require("./lib/Intern");
-// const Manager = require("./lib/Manager");
 const fs = require('fs');
-
-// const num = 0;
 const team = [];
+let role = "";
 
-// Initiate terminal based user interface
 
-async function start_question() {
-    // num++
+async function start_questions() {
     inquirer.prompt([
         // Ask user to input username
         {
             type: "input",
             name: "name",
-            message: "What is your name?"
+            message: "What is your name (Manager)?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your email address?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is your ID number?"
+        },
+        {
+            type: "input",
+            name: "office_number",
+            message: "What is your office number?",
+        },
+        {
+            type: "input",
+            name: "team_name",
+            message: "What do you want as your team name?",
+        },
+        // Then Once those choices have been made
+    ]).then(async function (data) {
+        // const new_member = data.new_member;
+        
+        // const manager_confirm = data.manager_confirm
+        // const team_name = data.team_name;
+        const name = data.name;
+        const email = data.email;
+        const id = data.id;
+        const office_number = data.office_number;
+        const team_name = data.team_name;
+        print(team_name)
+        role === 'Manager'
+        const manager_i = `<i class="fas fa-mug-hot"></i>`
+        const manager_bg_color = `card_d_m`
+        const manager_modifier = `Office Number: `
+        const new_manager = new Create_Team_Member(name, email, id, role, office_number, manager_i, manager_bg_color, manager_modifier, team_name);
+        await team.push(new_manager);
+        print(team)
+        // }
+        await loop_questions(team_name);
+    })
+
+};
+
+start_questions();
+
+// Initiate terminal based user interface
+async function loop_questions(team_name) {
+    inquirer.prompt([
+        // Ask user to input username
+        {
+            type: "input",
+            name: "name",
+            message: "What is your name (Employee)?"
         },
         {
             type: "input",
@@ -36,8 +86,6 @@ async function start_question() {
             name: "role",
             message: "What is your role in the team?",
             choices: [
-                "Manager",
-                // "Employee",
                 "Engineer",
                 "Intern"]
         },
@@ -60,108 +108,75 @@ async function start_question() {
             when: (data) => data.role === 'Engineer'
         },
         {
-            type: "input",
-            name: "team_name",
-            message: "What do you want as your team name?",
-            when: (data) => data.new_member === 'false'
-        },
-        {
             type: "confirm",
             name: "new_member",
             message: "Would you like to add another member?"
         },
         // Then Once those choices have been made
     ]).then(async function (data) {
-        // print(data)
         const name = data.name;
         const email = data.email;
         const id = data.id;
         const role = data.role;
         const school = data.school;
-        const office_number = data.office_number;
+        // const office_number = data.office_number;
         const username = data.username;
         const new_member = data.new_member;
-        // print(new_member)
-        // const team_name = data.team_name;
+        
         if (role === 'Intern') {
             const intern_i = `<i class="fas fa-graduation-cap"></i>`
             const intern_bg_color = `card_d_i`
             const intern_modifier = `Office Number: `
             const new_intern = new Create_Team_Member(name, email, id, role, school, intern_i, intern_bg_color, intern_modifier);
-            // const employee = new Intern(name, id, email, school);
             await team.push(new_intern);
-            // await html(name, email, id, role, school, team_name, "School: ", intern_i, intern_bg_color, team_object);
-            print(new_intern);
             print(team)
         }
-        else if (role === 'Manager') {
-            const manager_i = `<i class="fas fa-mug-hot"></i>`
-            const manager_bg_color = `card_d_m`
-            const manager_modifier = `Office Number: `
-            const new_manager = new Create_Team_Member(name, email, id, role, office_number, manager_i, manager_bg_color, manager_modifier);
-            // const employee = new Manager(name, id, email, office_number);
-
-            await team.push(new_manager);
-            // print(new_manager);
-            print(team)
-            // await html(name, email, id, role, office_number, team_name, "Office Number: ", manager_i, manager_bg_color, team_object);
-        }
+        // else if (role === 'Manager') {
+        //     const manager_i = `<i class="fas fa-mug-hot"></i>`
+        //     const manager_bg_color = `card_d_m`
+        //     const manager_modifier = `Office Number: `
+        //     const new_manager = new Create_Team_Member(name, email, id, role, office_number, manager_i, manager_bg_color, manager_modifier);
+        //     await team.push(new_manager);
+        //     print(team)
+        // }
         else if (role === 'Engineer') {
-
-            // print(new_engineer);
             const engineer_i = `<i class="fas fa-ruler-combined"></i>`
             const engineer_bg_color = `card_d_e`
             const engineer_modifier = `GitHub: `
             const new_engineer = new Create_Team_Member(name, email, id, role, username, engineer_i, engineer_bg_color, engineer_modifier);
-            // const employee = new Engineer(name, id, email, office_number);
             await team.push(new_engineer);
             print(team)
-            // await html(name, email, id, role, username, team_name, "GitHub: ", engineer_i, engineer_bg_color, team_object);
         }
-
         if (new_member) {
-            await start_question()
+            await loop_questions(team_name)
         }
         else if (new_member === false) {
-            // print(team)
-            // print(team.length)
-            loop_through_array(team)
-
+            await loop_through_array(team)
             print("Done")
             await finish_html();
         }
     })
 };
 
-async function loop_through_array() {
-    await create_html();
+async function loop_through_array(team_name) {
+    await create_html(team[0].team_name);
     for (let i = 0; i < team.length; i++) {
-        // print(team[i])
         const name = team[i].name;
-        // print(icon)
         const email = team[i].email;
         const id = team[i].id;
         const role = team[i].role;
         const icon = team[i].icon;
-        // print(icon)
         const background_color = team[i].background_color;
         const modifier = team[i].modifier;
         const user_specs = team[i].user_specs;
-        // const office_number = i.office_number;
-        // const username = i.username;
-        // const new_member = i.new_member;
-        // await html(name, email, id, role, icon, background_color, modifier, user_specs);
         await append_html(name, email, id, role, icon, background_color, modifier, user_specs)
     }
-    // await finish_html();
-
 }
 
 
-start_question();
 
-
-function Create_Team_Member(name, email, id, role, user_specs, icon, background_color, modifier) {
+function Create_Team_Member(name, email, id, role, user_specs, icon, background_color, modifier, team_name) {
+    this.team_name = team_name
     this.name = name;
     this.email = email;
     this.id = id;
@@ -170,59 +185,12 @@ function Create_Team_Member(name, email, id, role, user_specs, icon, background_
     this.background_color = background_color;
     this.modifier = modifier;
     this.user_specs = user_specs;
-    // if (role === 'Intern') {
-    //     this.user_specs = user_specs;
-    //     // const file_name = "intern"
-    //     // read_template(file_name);
-    // }
-    // else if (role === 'Manager') {
-    //     this.user_specs = user_specs;
-    //     // const file_name = "manager"
-    //     // read_template(file_name);
-    // }
-    // else if (role === 'Engineer') {
-    //     this.user_specs = user_specs;
-    //     // const file_name = "engineer"
-    //     // read_template(file_name);
-    // }
-    // // this.team_object
 }
 
-// function read_template(file_name) {
-//     fs.readFile(`./templates/${file_name}.html`, "utf8", function (error, data) {
-
-//         if (error) {
-//             return console.log(error);
-//         }
-
-//         console.log(data);
-//         html(data);
-//     });
-// }
-
-// async function html(name, email, id, role, icon, background_color, modifier, user_specs) {
-//     // print(team_object)
-//     var bg_modifier = bg_modifier;
-//     if (fs.existsSync('output/team.html')) {
-//         await append_html(name, email, id, role, icon, background_color, modifier, user_specs)
-//         // print(bg_modifier)
-//         print("File Does Exist")
-//     }
-//     else {
-//         await create_html(name, email, id, role, icon, background_color, modifier, user_specs);
-//         // print(bg_modifier)
-//         print("File Does Not Exist")
-//     }
-// }
-
 // Function to create the pdf from the github information
-async function create_html() {
-    // Progress Message
-    // print("Almost...\n");
-    // Try the following things
+async function create_html(team_name) {
 
     try {
-        // print(bg_modifier)
         const header_html = `
 <!DOCTYPE html>
 <html lang="en">
@@ -233,12 +201,12 @@ async function create_html() {
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://kit.fontawesome.com/cc10a71280.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../css/style.css">
-    <title>Manager</title>
+    <title>Team Profile Generator</title>
 </head>
 
 <body>
 <header>
-    <h1>Team Name</h1> 
+    <h1>${team_name}</h1> 
 </header>
 <main>`
         await fs.writeFile('output/team.html', header_html, (error) => {
@@ -246,10 +214,6 @@ async function create_html() {
                 print(error)
             }
         });
-
-        // await append_html(name, email, id, role, icon, background_color, modifier, user_specs);
-
-
         // If there is an error catch it
     } catch (e) {
         print(e);
@@ -257,9 +221,6 @@ async function create_html() {
 }
 
 async function append_html(name, email, id, role, icon, background_color, modifier, user_specs) {
-    // const html = data
-
-    // const engineer_i = `<i class="fas fa-ruler-combined"></i>`
     const html = `
 <div id="container_d">
     <div id="${background_color}">
@@ -290,6 +251,8 @@ async function append_html(name, email, id, role, icon, background_color, modifi
 }
 
 
+
+
 async function finish_html() {
     const footer_html = `
 </main>
@@ -303,7 +266,10 @@ async function finish_html() {
     });
 
 }
-// Hi Kurt so long omg 
+
+
+
+
 // Python print function
 function print(x) {
     console.log(x)
